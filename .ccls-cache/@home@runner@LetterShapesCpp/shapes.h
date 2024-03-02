610df5fe397d2cc9d95d640f6ef76ba50238fb9d
Zach <zach@fauser.net>
2024-03-02 00:17:13 +0000

    edit gitignore

--- .ccls-cache/@home@runner@LetterShapesCpp/shapes.h
+++ .ccls-cache/@home@runner@LetterShapesCpp/shapes.h
@@ -1,6 +1,8 @@
 #ifndef SHAPES_H
 #define SHAPES_H
 
+void printSquare(int userNumber, char userLetter);
+void printHollowSquare(int userNumber, char userLetter);
 void printRightTriangle(int userNumber, char userLetter);
 
 #endif
