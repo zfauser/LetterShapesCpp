9549615cab6e5a1bb2a54d7e32f2d19c89e13c0e
Zach <19974370+Zfauser@users.noreply.github.com>
2024-03-02 10:43:29 -0500

    remove unnecessary string arguments, further error trapped whatShape(), and added comments

--- inputs.cpp
+++ inputs.cpp
@@ -1,85 +1,145 @@
-#include "inputs.h"
-#include "colors.h"
+/*
+Name: Zach Fauser
+Date: March 1st, 2024
+Purpose: The purpose of this file is to define the functions that will be used to handle the user's inputs
+*/
+
 #include <iostream>
 #include <limits>
 #include <string>
 
+// Include the inputs.h file so that the functions can be used in the main.cpp file
+#include "inputs.h"
+
+// Include the colors.h file so that the colors can be used in this program
+#include "colors.h"
+
 using namespace std;
 
-bool invalidCharacterFound(string inputNumber) {
-  for (int i = 0; i < inputNumber.length(); i++) {
-    if (!isdigit(inputNumber[i])) {
+bool invalidCharacterFound(string inputNumber)
+/*
+  Args:
+    inputNumber (string): The number that the user has inputted as a string
+  Returns:
+    bool: Returns true if the inputNumber contains an invalid character, and false if it does not
+*/
+{
+  // For the length of the inputNumber, make sure each character is a number
+  for (int i = 0; i < inputNumber.length(); i++)
+  {
+    if (!isdigit(inputNumber[i]))
+    {
       return true;
     }
   }
   return false;
 }
 
-int enterNumber(string inputNumber) {
+int enterNumber()
+/*
+  Returns:
+    outputNumber (int): Returns the inputNumber (string) variable as an integer, after it have been error trapped
+*/
+{
+  string inputNumber = "0";
+  // Ask the user to input a number
   cout << GREEN << "End a number: " << RESET;
   cin >> inputNumber;
-  while (cin.fail() || invalidCharacterFound(inputNumber) ||
-         stoi(inputNumber) <= 0) {
+
+  // While the input is invalid, ask the user to input a number again
+  while (cin.fail() || invalidCharacterFound(inputNumber) || stoi(inputNumber) <= 0)
+  {
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     cout << BOLD << RED
          << "Invalid input. Please enter a positive integer: " << RESET;
     cin >> inputNumber;
   }
+  // Convert the inputNumber to an integer
   int outputNumber = stoi(inputNumber);
   return outputNumber;
 }
 
-// Asks the user to input a letter
-
-char enterCharacter(string inputLetter) {
+char enterCharacter()
+/*
+  Returns:
+    outputLetter (char): Returns the inputLetter (string) variable as a character, after it have been error trapped
+*/
+{
+  string inputLetter = "0";
+  // Ask the user to input a character
   cout << GREEN << "Enter a character: " << RESET;
   cin >> inputLetter;
-  // While the input the user has put in has failed or is over one character,
-  // try again
-  while ((cin.fail()) || (inputLetter.length() != 1)) {
+
+  // While the input is invalid, ask the user to input a character again
+  while ((cin.fail()) || (inputLetter.length() != 1))
+  {
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
-    if (inputLetter.length() != 1) {
-      cout << BOLD << RED
-           << "Invalid input. Please only enter one character: " << RESET;
-    } else {
-      cout << BOLD << RED
-           << "Invalid input. Please enter a different character: " << RESET;
+
+    // If the input is not a single character, ask the user to only enter one character
+    if (inputLetter.length() != 1)
+    {
+      cout << BOLD << RED << "Invalid input. Please only enter one character: " << RESET;
+    }
+    // Otherwise, ask the user to enter a different character
+    else
+    {
+      cout << BOLD << RED << "Invalid input. Please enter a different character: " << RESET;
     }
     cin >> inputLetter;
   }
+  // Convert the inputLetter to a character
   char outputLetter = inputLetter[0];
   return outputLetter;
 }
 
-int whatShape(int inputShape) {
-  cout << GREEN
-       << "What shape would you like to print? \n1. Square \n2. Hollow Square "
-          "\n3. Right Angle Triangle\n4. Isosceles Triangle\nPlease enter a "
-          "number: "
+int whatShape()
+/*
+  Returns:
+    outputShape (int): Returns inputShape (string) variable as an integer after it has been error trapped
+*/
+{
+  string inputShape = "0";
+  // Ask the user to input a number for the shape they want
+  cout << GREEN << "What shape would you like to print? \n1. Square \n2. Hollow Square "
+                   "\n3. Right Angle Triangle\n4. Isosceles Triangle\nPlease enter a number: "
        << RESET;
   cin >> inputShape;
-  while (cin.fail() || inputShape < 1 || inputShape > 4) {
+  // While the input is invalid, ask the user to input a number again
+  while (cin.fail() || invalidCharacterFound(inputShape) || stoi(inputShape) < 1 || stoi(inputShape) > 4)
+  {
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(), '\n');
     cout << BOLD << RED
          << "Invalid input. Please enter a number between 1 and 4: " << RESET;
     cin >> inputShape;
   }
-  return inputShape;
+  // Convert the inputShape to an integer
+  int outputShape = stoi(inputShape);
+  return outputShape;
 }
 
-bool askPlayAgain(bool inputPlayAgain) {
+bool askPlayAgain()
+/*
+  Returns:
+    inputPlayAgain (bool): Returns true if the user wants to play again, and false if they do not
+*/
+{
+  bool inputPlayAgain;
   string userAnswer;
+  // Ask the user if they want to play again
   cout << GREEN << "Would you like to play again? (Y or N): " << RESET;
   cin >> userAnswer;
-  while (userAnswer != "Y" && userAnswer != "N" && userAnswer != "y" &&
-         userAnswer != "n") {
+  // While the input is invalid, ask the user to input a different answer
+  while (userAnswer != "Y" && userAnswer != "N" && userAnswer != "y" && userAnswer != "n")
+  {
     cout << BOLD << RED << "Invalid input. Please enter Y or N: " << RESET;
     cin >> userAnswer;
   }
-  if (userAnswer == "Y" || userAnswer == "y") {
+  // If the user wants to play again, set inputPlayAgain to true, otherwise set it to false
+  if (userAnswer == "Y" || userAnswer == "y")
+  {
     inputPlayAgain = true;
   } else {
     inputPlayAgain = false;
--- shapes.cpp
+++ shapes.cpp
@@ -1,36 +1,82 @@
-#include "shapes.h"
-#include "colors.h"
+/*
+Name: Zach Fauser
+Date: March 1st, 2024
+Purpose: The purpose of this file is to define the functions that will be used to display the different shapes
+*/
+
 #include <iostream>
 #include <limits>
 #include <string>
 
+// Include the shapes.h file so that the functions can be used in the main.cpp file
+#include "shapes.h"
+
+// Include the colors.h file so that the colors can be used in this program
+#include "colors.h"
+
 using namespace std;
 
-bool isEven(int userNumber) {
-  if (userNumber % 2 == 0) {
+bool isEven(int userNumber)
+/*
+  Args:
+    userNumber (int): The number that the user has inputted
+  Returns:
+    bool: Returns true if the number is even, and false if the number is odd
+*/
+{
+  // If the number has a remainder of 0 when divided by 2, then it is even
+  if (userNumber % 2 == 0)
+  {
     return true;
   } else {
     return false;
   }
 }
 
-void printSquare(int userNumber, char userLetter) {
-  for (int i = 0; i < userNumber; i++) {
-    for (int j = 0; j < userNumber; j++) {
+void printSquare(int userNumber, char userLetter)
+/*
+  Args:
+    userNumber (int): The number that the user has inputted
+    userLetter (char): The character that the user has inputted
+  Returns:
+    cout: Outputs the square to the console
+*/
+{
+  // For the number of times that the user has inputted, output the user's letter (height)
+  for (int i = 0; i < userNumber; i++)
+  {
+    // For the number of times that the user has inputted, output the user's letter (width)
+    for (int j = 0; j < userNumber; j++)
+    {
       cout << MAGENTA << userLetter << RESET;
     }
     cout << "\n";
   }
 }
 
-void printHollowSquare(int userNumber, char userLetter) {
-  for (int i = 0; i < userNumber; i++) {
-    for (int j = 0; j < userNumber; j++) {
-      // Outputs the user's letter for the Top, Bottom, and the Sides of the
-      // box, if it is none of those, output nothing
-      if (j == 0 || j == userNumber - 1 || i == 0 || i == userNumber - 1) {
+void printHollowSquare(int userNumber, char userLetter)
+/*
+  Args:
+    userNumber (int): The number that the user has inputted
+    userLetter (char): The character that the user has inputted
+  Returns:
+    cout: Outputs the hollow square to the console
+*/
+{
+  // For the number of times that the user has inputted, output the user's letter (height)
+  for (int i = 0; i < userNumber; i++)
+  {
+    // For the number of times that the user has inputted, output the user's letter (width)
+    for (int j = 0; j < userNumber; j++)
+    {
+      // Outputs the user's letter for the Top, Bottom, and the Sides of the box
+      if (j == 0 || j == userNumber - 1 || i == 0 || i == userNumber - 1)
+      {
         cout << CYAN << userLetter << RESET;
-      } else {
+      }
+      // Outputs a space for the inside of the box
+      else
+      {
         cout << " ";
       }
     }
@@ -38,26 +84,58 @@ void printHollowSquare(int userNumber, char userLetter) {
   }
 }
 
-void printRightTriangle(int userNumber, char userLetter) {
-  for (int i = 0; i < userNumber; i++) {
-    for (int j = 0; j <= i; j++) {
+void printRightTriangle(int userNumber, char userLetter)
+/*
+  Args:
+    userNumber (int): The number that the user has inputted
+    userLetter (char): The character that the user has inputted
+  Returns:
+    cout: Outputs a right angle triangle to the console
+*/
+{
+  // For the number of times that the user has inputted, output the user's letter (height)
+  for (int i = 0; i < userNumber; i++)
+  {
+    // For the the same number of times as the current height being outputted, output the user's letter
+    for (int j = 0; j <= i; j++)
+    {
       cout << YELLOW << userLetter << RESET;
     }
     cout << "\n";
   }
 }
 
-void printIsoscelesTriangle(int userNumber, char userLetter) {
-  for (int i = 0; i < userNumber; i += 2) {
-    for (int j = 0; j < (userNumber - i) / 2; j++) {
+void printIsoscelesTriangle(int userNumber, char userLetter)
+/*
+  Args:
+    userNumber (int): The number that the user has inputted
+    userLetter (char): The character that the user has inputted
+  Returns:
+    cout: Outputs an isosceles triangle to the console
+*/
+{
+  // Outputs the appropriate number of layers for the triangle
+  for (int i = 0; i < userNumber; i += 2)
+  {
+    // Output spaces for the left side of the triangle
+    for (int j = 0; j < (userNumber - i) / 2; j++)
+    {
       cout << " ";
     }
-    if (isEven(userNumber)) {
-      for (int k = 0; k <= i + 1; k++) {
+    // If the base number is even...
+    if (isEven(userNumber))
+    {
+      // Output one extra character for even userNumbers
+      for (int k = 0; k <= i + 1; k++)
+      {
         cout << BLUE << userLetter << RESET;
       }
-    } else {
-      for (int k = 0; k <= i; k++) {
+    }
+    else
+    {
+      // Output the same number of characters as the current i value
+      for (int k = 0; k <= i; k++)
+      {
         cout << BLUE << userLetter << RESET;
       }
     }
