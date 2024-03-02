e94c83cb86c3a0357f71c9d8571d9e5bc3b91eab
Zach <19974370+Zfauser@users.noreply.github.com>
2024-03-01 19:49:45 -0500

    fixed?

--- .ccls-cache/@home@runner@LetterShapesCpp/shapes.h
+++
@@ -1,8 +0,0 @@
-#ifndef SHAPES_H
-#define SHAPES_H
-
-void printSquare(int userNumber, char userLetter);
-void printHollowSquare(int userNumber, char userLetter);
-void printRightTriangle(int userNumber, char userLetter);
-
-#endif
