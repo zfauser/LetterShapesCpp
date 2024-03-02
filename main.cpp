4dc432988c3ff89c2180a9a85642888459ecd998
Zach <19974370+Zfauser@users.noreply.github.com>
2024-03-01 19:13:19 -0500

    add gitignore

--- main.cpp
+++ main.cpp
@@ -75,49 +75,6 @@ int whatShape(int inputShape)
   return inputShape;
 }
 
-void printSquare(int userNumber, char userLetter)
-{
-  for (int i = 0; i < userNumber; i++)
-  {
-    for (int j = 0; j < userNumber; j++)
-    {
-      cout << userLetter;
-    }
-    cout << "\n";
-  }
-}
-
-void printHollowSquare(int userNumber, char userLetter)
-{
-  for (int i = 0; i < userNumber; i++)
-  {
-    for (int j = 0; j < userNumber; j++)
-    {
-      // Outputs the user's letter for the Top, Bottom, and the Sides of the box, if it is none of those, output nothing
-      if (j == 0 || j == userNumber - 1 || i == 0 || i == userNumber - 1)
-      {
-        cout << userLetter;
-      }
-      else
-      {
-        cout << " ";
-      }
-    }
-    cout << "\n";
-  }
-}
-
-// void printRightTriangle(int userNumber, char userLetter)
-// {
-//   for (int i = 0; i < userNumber; i++)
-//   {
-//     for (int j = 0; j <= i; j++)
-//     {
-//       cout << userLetter;
-//     }
-//     cout << "\n";
-//   }
-// }
 
 bool isEven(int userNumber) {
   if (userNumber % 2 == 0) {
--- shapes.cpp
+++ shapes.cpp
@@ -5,6 +5,39 @@
 
 using namespace std;
 
+void printSquare(int userNumber, char userLetter)
+{
+  for (int i = 0; i < userNumber; i++)
+  {
+    for (int j = 0; j < userNumber; j++)
+    {
+      cout << userLetter;
+    }
+    cout << "\n";
+  }
+}
+
+
+void printHollowSquare(int userNumber, char userLetter)
+{
+  for (int i = 0; i < userNumber; i++)
+  {
+    for (int j = 0; j < userNumber; j++)
+    {
+      // Outputs the user's letter for the Top, Bottom, and the Sides of the box, if it is none of those, output nothing
+      if (j == 0 || j == userNumber - 1 || i == 0 || i == userNumber - 1)
+      {
+        cout << userLetter;
+      }
+      else
+      {
+        cout << " ";
+      }
+    }
+    cout << "\n";
+  }
+}
+
 void printRightTriangle(int userNumber, char userLetter)
 {
   for (int i = 0; i < userNumber; i++)
